a9a13a1683921159ee7b284dd887aae3c4f82231
mrezanomani <mrezanomani@users.noreply.github.com>
2025-12-04 22:55:44 +0330

    create database

--- Modules/Acl/database/migrations/2025_12_04_185528_create_permission_role_table.php
+++ Modules/Acl/database/migrations/2025_12_04_185528_create_permission_role_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,11 @@ return new class extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('permission_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
+            $table->unique(['role_id', 'permission_id']);
         });
     }
 
--- Modules/Acl/database/migrations/2025_12_04_185528_create_role_user_table.php
+++ Modules/Acl/database/migrations/2025_12_04_185528_create_role_user_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,13 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
+            $table->unique(['role_id', 'user_id']);
         });
+
     }
 
     /**
